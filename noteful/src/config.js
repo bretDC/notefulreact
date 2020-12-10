4a90b9ffc1123419751eed85bc06ea7122bb78fa
bretThinkful <bretbickhamx1989@gmail.com>
2020-12-10 09:52:12 -0700

    a

--- noteful/src/config.js
+++ noteful/src/config.js
@@ -1,4 +1,5 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
 
-  API_ENDPOINT: 'http://localhost:8000/api'
+  API_ENDPOINT: 'http://localhost:3000/api'
 }
--- noteful/src/notes-helpers.js
+++ noteful/src/notes-helpers.js
@@ -7,8 +7,8 @@ export const findNote = (notes=[], noteId) =>
 export const getNotesForFolder = (notes=[], folderId) => (
   (!folderId)
     ? notes
-    : notes.filter(note => note.folderId === folderId)
+    : notes.filter(note => note.folder_id === folderId)
 )
 
 export const countNotesForFolder = (notes=[], folderId) =>
-  notes.filter(note => note.folderId === folderId).length
+  notes.filter(note => note.folder_id === folderId).length
